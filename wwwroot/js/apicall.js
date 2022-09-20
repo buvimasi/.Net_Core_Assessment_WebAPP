06bab0b9703ce7efbb06efc7b1141bda82056920
Buvaneshwari Masilamani <BuvaneshwariM@softura.com>
2022-09-20 14:07:33 +0530

    task3

---
+++ wwwroot/js/apicall.js
@@ -0,0 +1,32 @@
+﻿$(document).ready(
+    function () {
+        $('#submit').click(function () {
+
+
+            $.ajax({
+                url: 'http://localhost:5000/api/user/authenticate',
+                dataType: 'json',
+                type: 'post',
+                contentType: 'application/json',
+                data: JSON.stringify({
+                    username: $('#uname').val(),
+                    password: $('#psw').val()
+                }),
+                processData: false,
+                success: function (data, textStatus, jQxhr) {
+                    alert("User successfully authenticated")
+                    window.location.replace('~/Pages//Manager.html')
+
+                },
+                error: function (jqXhr, textStatus, errorThrown) {
+                    alert("User Failed to authenticate")
+                    window.location.replace('~/Pages//Wfm_Manager.html')
+                }
+            });
+
+
+
+        })
+
+    }
+)
