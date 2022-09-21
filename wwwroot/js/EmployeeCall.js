8717618a99ab1666fb97f3f28129f20b611d3b96
Buvaneshwari Masilamani <BuvaneshwariM@softura.com>
2022-09-21 17:37:06 +0530

    task5

---
+++ wwwroot/js/EmployeeCall.js
@@ -0,0 +1,18 @@
+﻿$.get("https://localhost:7201/api/Employee/GetEmployee", function (data, status) {
+
+    if (data) {
+        let code = ""
+        for (let x in data) {
+            code += "<tr>"
+            code += "<td>" + data[x].Name + "</td>"
+            code += "<td>" + data[x].Email + "</td>"
+            code += "<td>" + data[x].Manager + "</td>"
+            code += "<td>" + data[x].Wfm_Manager + "</td>"
+            code += "<td>" + data[x].Status + "</td>"
+
+            code += "</tr>"
+        }
+        $("#tdata").html(code)
+    }
+
+})
